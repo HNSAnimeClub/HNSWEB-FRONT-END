daa715d50aec6183c8cf88278a7c219aea919923
米洛 <kiritoasu@163.com>
2022-06-15 18:30:43 +0800

    feat:新增HNSButton 组件

---
+++ src/component/testArea/Test.jsx
@@ -0,0 +1,22 @@
+/**
+ * @name: Test.jsx
+ * @author: 米洛
+ * @date: 2022-06-13 13:16
+ * @description：用于组件测试的沙盒（试验田）
+ */
+
+
+
+import React from 'react';
+import HNSButton from "../baseUI/hnsButton/HNSButton";
+import style from './test.module.less'
+
+function Test(props) {
+  return (
+    <div className={style.container}>
+      <HNSButton type="default" color={"#faad14"}>HNSButton</HNSButton>
+    </div>
+  );
+}
+
+export default Test;
