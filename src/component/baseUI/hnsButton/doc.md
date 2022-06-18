11152505d3df44e15bd436f8e2d1fff016f572e1
米洛 <kiritoasu@163.com>
2022-06-18 23:22:33 +0800

    chore:完善HNS-Button组件的部分逻辑于文档

---
+++ src/component/baseUI/hnsButton/doc.md
@@ -0,0 +1,19 @@
+# HNS Button 组件
+
+  <a>
+    <img src="https://img.shields.io/badge/HNS--Button-v1.0-%23ff6987" alt="HNS-Button">
+  </a>
+
+**可传参数**
+
+| 参数名  |   类型   |        可选         | 必传  |   默认值   |
+|:----:|:------:|:-----------------:|:---:|:-------:|
+| type | String | primary / default |  否  | default |
+
+**使用示例：**
+
+```jsx
+<HNSButton type={"primary"} onClick={handleClick}>按钮</HNSButton>
+```
+
+动画和样式 通过切换不同的 `className` 实现不同的效果
--- src/component/pages/example/Example.jsx
+++ src/component/pages/example/Example.jsx
@@ -5,8 +5,8 @@
  */
 
 import React, {useState} from 'react';
-import {Button} from "antd";
 import style from './example.module.less'
+import HNSButton from "../../baseUI/hnsButton/HNSButton";
 
 function Example(props) {
   const [state, setState] = useState(1)
@@ -17,7 +17,7 @@ function Example(props) {
   return (
     <div className={style.container}>
       <p>{(state && "你好，世界") || "这是你的第一个 React 组件"}</p>
-      <Button onClick={handleClick}>点击切换文本</Button>
+      <HNSButton onClick={handleClick} >点击切换文本</HNSButton>
     </div>
   );
 }
