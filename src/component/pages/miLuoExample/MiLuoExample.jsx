936c56e40b611829223bda62a04562569be2ce7d
米洛 <kiritoasu@163.com>
2022-07-03 15:50:20 +0800

    fix:修复某些莫名其妙的bug

---
+++ src/component/pages/miLuoExample/MiLuoExample.jsx
@@ -0,0 +1,29 @@
+/**
+ * @name: MiLuoExample.jsx
+ * @author: 米洛
+ * @date: 2022-07-03 15:21
+ * @description：米洛的示例组件
+ */
+
+// import React from 'react';
+//
+// function MiLuoExample(props) {
+//   return (
+//     <div></div>
+//   );
+// }
+//
+// export default MiLuoExample;
+
+
+import React from 'react'
+
+function MiLuoExample() {
+  return (
+    <div>
+      米洛的组件
+    </div>
+  )
+}
+
+export default MiLuoExample
