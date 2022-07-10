63557f7cf8d509116dd66d38c80c71917f55de23
未知fu号 <3292787736@qq.com>
2022-07-10 17:54:26 +0800

    test

---
+++ src/component/pages/wzFuHaoExample/WzFuHaoExample002.jsx
@@ -0,0 +1,29 @@
+/**
+ * @name: WzFuHaoExample
+ * @author: WzFuHao
+ * @date: @date: 2022-07-10 17:06
+ * @description：符号的示例文件
+ * @update: 2022-07-10 15:28
+ */
+
+import React, {useState} from "react";
+import style from './wzFuHaoExample002.module.less'
+
+function WzFuHao002() {
+    const [Content, setContent] = useState("")
+
+
+    const handleChange = (e) => {
+        setContent(e.target.value)
+    }
+
+
+    return (
+        <div className={style.container}>
+            <input onChange={handleChange}/>
+            <h1>{Content}</h1>
+        </div>
+    )
+}
+
+export default WzFuHao002
